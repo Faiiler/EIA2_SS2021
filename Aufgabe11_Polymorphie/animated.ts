ce4d7fe6763a225e6b7f7781c12fbe48287d1e8f
Max <maximilian.buckel@hs-furtwangen.de>
2021-06-14 12:54:11 +0200

    Updated info

--- Aufgabe11_Polymorphie/animated.ts
+++ Aufgabe11_Polymorphie/animated.ts
@@ -4,3 +4,11 @@ namespace eia10_2 {
         draw(): void {}
     }
 }
+
+/*
+    Aufgabe: L10.2_Polymorphie
+    Name: Maximilian Buckel
+    Matrikel: 266830
+    Datum: 12.06.2021
+    Quellen: Larissa Gaede, Eva Breuninger
+    */
